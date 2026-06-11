e8e2573799d94bbd8dde63114b01b46d86588b3f
GLB-25110CN258 <anshikakatiyar693@gmail.com>
2026-06-11 16:14:19 +0530

    Day-11 assignment is uploaded

---
+++ Day-11/Q43.cpp
@@ -0,0 +1,38 @@
+
+// Write a program to write function to check prime
+
+#include <iostream>
+using namespace std;
+
+int prime(int num)
+{
+    if(num<=1)
+    {
+        return 0;
+    }
+    for(int i=2 ; i<num ; i++)
+    {
+        if(num%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int number;
+
+    cout<<"Enter the number = ";
+    cin>>number;
+
+    if(prime(number)==1)
+    {
+        cout<<number<<" is a prime number";
+    }
+    else
+    {
+        cout<<number<<" is not a prime number";
+    }
+    return 0;
+}
